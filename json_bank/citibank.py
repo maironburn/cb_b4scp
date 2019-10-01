c22e2cdf63238e28e14ea6afe9d0def3fda05243
mario.diaz.rodriguez <Md14z1s0trol>
2019-10-01 20:03:51 +0200

    empezando automatismo img2

--- json_bank/citibank.py
+++ json_bank/citibank.py
@@ -1,7 +1,7 @@
 citibank = {
 
     'bankname': 'citibank',  # redireccion al banco de santander,
-    'browser_driver' : 'iexplorer',
+    'browser_driver': 'iexplorer',
     # por si se requiere construir url a partir de otros params o url relativas
     'url_base': 'https://portal.citidirect.com/portalservices/forms/login.pser?',
     'headless': False,  # @todo
@@ -54,7 +54,8 @@ citibank = {
          'mode': 'click', 'description': 'Boton de consultar'}
 
     ],
-    'img_recognition_workflow': [
+    'img_recognition_workflow_ori': [
+
         {'tipo': 'xpath', 'target': "//input[@name='seuNumero']",
          'mode': 'fill', 'data': 'seaNumero', 'description': 'Seu numero'},
 
@@ -66,6 +67,53 @@ citibank = {
 
         {'tipo': 'xpath', 'target': "//form[@name='form2']//input[@name='btnConfirmar']",
          'mode': 'click', 'description': 'boton OK'}
+    ],
+
+    'img_recognition_workflow': [ {'Collection_Item_Detail': [
+
+        {'target': 'product', 'action': "click"},
+        {'target': 'combo_product', 'action':  "click"},
+
+        {'target': 'original_amount', 'action':  "fill" , 'data': ''},
+
+        {'target': 'type_collection_item',  'action':  "click"},
+        {'target': 'combo_type_collection_item', 'action':  "click"},
+
+        {'target': 'combo_type_collection_item', 'action':  "click"},
+
+        {'target': 'emission_date', 'action':  "fill" , 'data': ''},
+
+        {'target': 'customer_reference', 'action': "fill", 'data': ''},
+
+        {'target': 'due_date', 'action': "fill", 'data': ''},
+
+        {'target': 'collection_item_doc_type',  'action':  "click"},
+        {'target': 'combo_collection_item_doc_type',  'action':  "click"},
+
+        {'target': 'allow_divergent',  'action':  "click"},
+        {'target': 'combo_allow_divergent',  'action':  "click"},
+
+        {'target': 'payer_name', 'action': "fill", 'data': ''},
+
+        {'target': 'payer_type', 'action': "click"},
+        {'target': 'combo_payer_type', 'action': "click"},
+        #@todo, esto puede ser bte jodido
+        {'target': 'payer_type_txtbox', 'action': "fill", 'data': ''},
+
+        {'target': 'payer_name', 'action': "fill", 'data': ''},
+
+        {'target': 'payer_address', 'action': "fill", 'data': ''},
+
+        {'target': 'payer_city', 'action': "fill", 'data': ''},
+
+        {'target': 'payer_state', 'action': "fill", 'data': ''},
+
+        {'target': 'payer_zipcode', 'action': "fill", 'data': ''},
+
+        {'target': 'Collection_Item_Detail_btn_submit', 'action': "click"},
+
+        ]}
     ]
 
+#end json
 }
--- screen_schemes/windows_skels.py
+++ screen_schemes/windows_skels.py
@@ -1,6 +1,23 @@
 import os.path
 from common_config import TEMP_IMGS, IMGS_DATASET
 
+
+warning_msg = {
+    "_name": "warning_msg",
+    "_parent":None,
+    "_img_folder": os.path.join(IMGS_DATASET, 'Warning_msg'),
+    "_dict_elements": {'btn_yes': '', 'btn_no': ''}
+}
+
+Russian_msg = {
+    "_name": "Russian_msg",
+    "_parent": None,
+    "_img_folder":  os.path.join(IMGS_DATASET, 'Russian_msg'),
+    "_dict_elements": {'btn_ok': ''}
+}
+
+
+
 Collection_Item_Detail = {
     "_name": "Collection_Item_Detail",
     "_parent": None,
@@ -16,33 +33,8 @@ Collection_Item_Detail = {
                        }
 }
 
-warning_msg = {
-    "_name": "warning_msg",
-    "_parent":None,
-    "_img_folder": os.path.join(IMGS_DATASET, 'Warning_msg'),
-    "_dict_elements": {'btn_yes': '', 'btn_no': ''}
-}
 
-Russian_msg = {
-    "_name": "Russian_msg",
-    "_parent": None,
-    "_img_folder":  os.path.join(IMGS_DATASET, 'Russian_msg')
-    "_dict_elements": {'btn_ok': ''}
-}
 
-popscreen_add_telf = {
-    "_name": "popscreen_add_telf",
-    "_parent": "nuevo_declarante",
-    "_img_folder": "{}{}{}{}{}".format(nuevo_declarante['_img_folder'], os.path.sep, 'telf_email', os.path.sep,
-                                       'popscreen_add_telf'),
-    "_dict_elements": {}
-}
 
-popscreen_add_domicilio = {
-    "_name": "popscreen_add_domicilio",
-    "_parent": "nuevo_declarante",
-    "_img_folder": "{}{}{}{}{}".format(nuevo_declarante['_img_folder'], os.path.sep, 'domicilio', os.path.sep,
-                                       'popscreen_add_domicilio'),
-    "_dict_elements": {}
-}
+
 
